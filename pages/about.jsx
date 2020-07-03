35e50c9b81774e9768d2228a19083cf3b50b5d0b
cagriakar <55461100+cagriakar@users.noreply.github.com>
2020-07-03 17:33:50 +0300

    git clone boilerplate

---
+++ pages/about.jsx
@@ -0,0 +1,46 @@
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import {
+    useDispatch,
+    useStateValues
+} from '../components/context/GlobalStateProvider';
+import Copyright from '../src/Copyright';
+import Link from '../src/Link';
+import ProTip from '../src/ProTip';
+
+export default function About() {
+    const { count } = useStateValues();
+    const { handleIncrease, handleIncrease15 } = useDispatch();
+
+    return (
+        <Container maxWidth="sm">
+            <p>Counter: {count}</p>
+            <button type="button" onClick={handleIncrease}>
+                Increase
+            </button>
+            <button type="button" onClick={handleIncrease15}>
+                Increase By 15
+            </button>
+
+            <Box my={4}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Next.js example
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    component={Link}
+                    naked
+                    href="/"
+                >
+                    Go to the main page
+                </Button>
+                <ProTip />
+                <Copyright />
+            </Box>
+        </Container>
+    );
+}
